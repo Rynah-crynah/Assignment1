bdd0049989d9ac44e50b0b37522ac43eb9546701
Rynah-Crynah <swabrinyusufshamun@gmail.com>
2022-02-18 19:57:59 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+
+    fun main() {
+        val name : String = "Ninah Shamun"
+        println (name)
+        val age = 28
+        println(age)
+        val phoneNumber : String = "0739587645"
+        println(phoneNumber)
+        val weight : Double = 49.7
+        println(weight)
+        val kenyanCitizen : Boolean = false
+        println(kenyanCitizen)
+
+    }
